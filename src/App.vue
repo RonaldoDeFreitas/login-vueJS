3d09dcbc6368aaebbfb5917f835c06c9e1a1c7b3
RonaldoDeFreitas <freitasderonaldo@gmail.com>
2019-07-04 19:12:00 -0300

    primeiro login

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+	
+  import VedAuth from './features/auth/main'
+
+	export default {
+    components: {
+      VedAuth
+    }
+  };
+</script>
+
+<template>
+  <ved-auth />
+</template>
+
---
+++ src/features/auth/main.vue
@@ -0,0 +1,78 @@
+<script>
+  import SignIn from "./sign-in";
+  import SignUp from "./sign-up";
+
+  export default {
+    components: {
+      SignIn,
+      SignUp
+    },
+    
+    data () {
+      return {
+        navigation: 'signin'
+      }
+    },
+
+    methods: {
+      navigate () {
+        this.$bus.$emit('navigate', this.navigation)
+        
+      },
+      doSignIn (obj) {
+        
+        //fingir que há uma chamada AJAX
+        window.console.log(obj)
+
+      },
+      doSignUp (obj) {
+        
+        //fingir que há uma chamada AJAX
+        window.console.log(obj)
+
+      }
+    }
+  };
+</script>
+
+
+<template>
+  <div>
+    <div class="login-wrap">
+      <div class="login-html">
+
+        <input 
+          id="tab-1" 
+          type="radio"
+          value="signin"
+          @change="navigate"
+          v-model="navigation"
+          name="tab" 
+          class="sign-in" 
+          checked
+        />
+        <label for="tab-1" class="tab">Sign In</label>
+
+        <input 
+          id="tab-2" 
+          type="radio"
+          value="signup"
+          @change="navigate"
+          v-model="navigation" 
+          name="tab" 
+          class="sign-up"
+        />
+        <label for="tab-2" class="tab">Sign Up</label>
+
+        <div class="login-form">
+          <sign-in @do-sign-in="doSignIn" />
+          <sign-up @do-sign-in="doSignUp"/>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<style src="./styles.css"></style>
+
